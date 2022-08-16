2dd9f65d44e2cc4692ac8fa031c140ca35708f26
Marilokms <42029758+Marilok@users.noreply.github.com>
2022-08-16 11:03:01 +0200

    Update CustomMap.tsx

--- components/Map/CustomMap.tsx
+++ components/Map/CustomMap.tsx
@@ -73,7 +73,7 @@ export default function CustomMap() {
     <MapContainer
       id="map"
       className={"w-full h-screen	"}
-      center={[49.1918183, 16.6122742]}
+      center={[49.1930642, 16.6099019]}
       zoom={13}
     >
       <TileLayer
