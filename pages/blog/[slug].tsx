720919bf796e41593aca34108e4f337287073d2b
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-01-04 12:06:29 +0000

    md init

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,12 @@
+import { Title } from "@mantine/core";
+import LandingPageWrapper from "../../components/Layouts/LandingPage/LandingPageWrapper";
+
+export default function Karton() {
+  return <LandingPageWrapper>
+
+    <Title order={2}>
+        
+    </Title>
+  </LandingPageWrapper>;
+};
+
