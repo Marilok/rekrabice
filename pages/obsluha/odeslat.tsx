2d0e09cce5b44ff4cbcba54b5445da88eba1a5a8
Marilokms <marilokms@protonmail.com>
2023-04-17 20:53:57 +0000

    odeslat pin codes

--- pages/obsluha/odeslat.tsx
+++ pages/obsluha/odeslat.tsx
@@ -1,37 +1,51 @@
-
-import { Anchor, Button, CopyButton, List, Tooltip } from "@mantine/core";
-import { IconCheck, IconCopy } from '@tabler/icons-react';
+import { Anchor, Button, CopyButton, List, Text, Tooltip } from "@mantine/core";
+import { IconCheck, IconCopy } from "@tabler/icons-react";
 import ObsluhaWrapper from "../../components/Layouts/Obsluha/ObsluhaWrapper";
 
 export default function Obsluha() {
-    const RETURN_CODE = "970 32 390";
-    return (
-        <ObsluhaWrapper title="Obsluha">
-            <List type="ordered">
-                <List.Item>
-                Zavřete přepravku s uskladněnými ReKrabicemi
-                </List.Item>
-                <List.Item>
-                Do&nbsp;
-                    <Anchor href="https://admin.packeta.com/sign/in" target="_blank">
-systému Zásilkovny
-                        </Anchor>&nbsp;
-                        zadejte kód
-                <span className="font-bold"></span>
-                  <CopyButton value={RETURN_CODE} timeout={2000}>
-      {({ copied, copy }) => (
-        <Tooltip label={copied ? 'Kopírováno' : 'Kopírovat kód'} withArrow position="right">
-            <Button variant="subtle" color={copied ? 'green' : 'gray'}  rightIcon={copied ? <IconCheck size="1rem" /> : <IconCopy size="1rem" />} onClick={copy}>
-{RETURN_CODE}
-            </Button>
-        </Tooltip>
-      )}
-    </CopyButton>
-                </List.Item>
-                <List.Item>
-                Nalepte štítek na přepravku
-                </List.Item>
-            </List>
-            </ObsluhaWrapper>
-    );
-    }
+  const RETURN_CODE = "970 32 390";
+  return (
+    <ObsluhaWrapper title="Obsluha">
+      <Text>
+        Je už přepravka plná? Tak nám ji prosím odešlete tímto způsobem:
+      </Text>
+      <List type="ordered">
+        <List.Item>Zavřete přepravku s uskladněnými ReKrabicemi</List.Item>
+        <List.Item>
+          Do&nbsp;
+          <Anchor href="https://admin.packeta.com/sign/in" target="_blank">
+            systému Zásilkovny
+          </Anchor>
+          &nbsp; zadejte kód
+          <span className="font-bold"></span>
+          <CopyButton value={RETURN_CODE} timeout={2000}>
+            {({ copied, copy }) => (
+              <Tooltip
+                label={copied ? "Kopírováno" : "Kopírovat kód"}
+                withArrow
+                position="right"
+              >
+                <Button
+                  variant="subtle"
+                  color={copied ? "green" : "gray"}
+                  rightIcon={
+                    copied ? (
+                      <IconCheck size="1rem" />
+                    ) : (
+                      <IconCopy size="1rem" />
+                    )
+                  }
+                  onClick={copy}
+                >
+                  {RETURN_CODE}
+                </Button>
+              </Tooltip>
+            )}
+          </CopyButton>
+        </List.Item>
+        <List.Item>Nalepte štítek na přepravku</List.Item>
+        <List.Item>Vyndejte novou přepravku</List.Item>
+      </List>
+    </ObsluhaWrapper>
+  );
+}
