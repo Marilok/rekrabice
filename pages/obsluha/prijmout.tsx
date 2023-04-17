cf0110bf3a14caa257f7582178323da3d5060d70
Marilokms <marilokms@protonmail.com>
2023-04-17 20:54:08 +0000

    prijmout pin codes

--- pages/obsluha/prijmout.tsx
+++ pages/obsluha/prijmout.tsx
@@ -1,9 +1,50 @@
-
+import { Button, PinInput, Stack, Text } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import ObsluhaWrapper from "../../components/Layouts/Obsluha/ObsluhaWrapper";
 
 export default function Obsluha() {
-    return (
-        <ObsluhaWrapper title="Obsluha">
-            </ObsluhaWrapper>
-    );
-    }
+  const form = useForm<FormValues>({
+    initialValues: { pin: "" },
+
+    validate: {
+      pin: (value: string) =>
+        value.length < 8 ? "Označení má přesně 8 znaků." : null,
+    },
+
+    transformValues: (values) => ({
+      pin: values.pin.toLocaleUpperCase(),
+    }),
+  });
+
+  const handlePinChange = (value: string) => {
+    form.setValues({ pin: value.toUpperCase() });
+  };
+
+  return (
+    <ObsluhaWrapper title="Obsluha">
+      <form
+        onSubmit={form.onSubmit((values) => {
+          console.log(values);
+          form.reset();
+        })}
+      >
+        <Stack>
+          <Text>Zadejte 8-ciferný kód, který naleznete na ReKrabici:</Text>
+          <PinInput
+            length={8}
+            autoFocus
+            size="xl"
+            required
+            {...form.getInputProps("pin")}
+            onChange={(event) => handlePinChange(event)}
+          />
+          <Button type="submit">Vrátit ReKrabici</Button>
+        </Stack>
+      </form>
+    </ObsluhaWrapper>
+  );
+}
+
+interface FormValues {
+  pin: string;
+}
