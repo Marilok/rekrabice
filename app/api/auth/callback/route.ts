de6cb236bbef5ef2bdddaab9a2eeb41ac73ac2f0
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-06-02 18:45:24 +0000

    route eslint fix

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,8 +1,6 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
-
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
