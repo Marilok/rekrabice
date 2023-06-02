b9ea161691724fad839e0bb049340150f7448fc3
Marilokms <marilokms@protonmail.com>
2023-06-02 18:09:05 +0000

    remove unused code

--- next.config.js
+++ next.config.js
@@ -19,8 +19,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// })
-// module.exports = withBundleAnalyzer({});
