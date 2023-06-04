8c552d11814bc1b08e0362858d2503a11e5f9d15
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-06-04 14:32:07 +0000

    another supabase try

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -11,6 +11,5 @@ export async function GET(request: NextRequest) {
   }
 
   // URL to redirect to after sign in process completes
-  // return NextResponse.redirect(new URL("/system/prijmout", request.url));
-  return NextResponse.redirect("https://rekrabice.cz/system/prijmout");
+  return NextResponse.redirect(new URL("/system/prijmout", request.url));
 }
--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,9 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-//   const {
-//     data: { session },
-//   } = await supabase.auth.getSession()
-// console.log((await supabase.auth.getSession()).data)
-
-//   if (session) {
-//     if (req.nextUrl.pathname.match('/system')) {
-//       return NextResponse.rewrite(new URL('/system/prijmout', req.url));
-//     }
-//     return res
-//   }
-
-//   if (req.nextUrl.pathname === "/system/login") {
-//     return res
-//   }
-
-//   return NextResponse.redirect(new URL('/system/login', req.url))
   await supabase.auth.getSession()
   return res
 }
-
-// export const config = {
-//   matcher: "/system/:path*",
-// }
