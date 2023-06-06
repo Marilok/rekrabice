a71603d61840b621b88a5a0ba5d6541b92d94cc0
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-06-06 08:46:26 +0000

    lint fix

--- components/Contact/ContactIcons.tsx
+++ components/Contact/ContactIcons.tsx
@@ -1,19 +1,44 @@
 import {
   Box, createStyles, Stack, Text, ThemeIcon,
 } from "@mantine/core";
-import {
-  IconAt,
-  IconMapPin,
-  IconPhone,
-  IconSectionSign,
-  IconSun,
-} from "@tabler/icons-react";
 import React from "react";
+import { IMPRESSUM_DATA } from "../../data/impressumData";
 
 type ContactIconVariant = "white" | "gradient" | "legal";
 
 interface ContactIconStyles {
   variant: ContactIconVariant;
+}
+
+function getColorVariant(variant: any, theme: any) {
+  if (variant === "gradient") {
+    return theme.colors.gray[6];
+  } if (variant === "white") {
+    return theme.colors[theme.primaryColor][0];
+  }
+  return "";
+}
+
+function getBgVariant(variant: any, theme: any) {
+  if (variant === "gradient") {
+    return `linear-gradient(135deg, ${
+      theme.colors[theme.primaryColor][4]
+    } 0%, ${theme.colors[theme.primaryColor][6]} 100%)`;
+  } if (variant === "legal") {
+    return `linear-gradient(135deg, ${
+      theme.colors[theme.primaryColor][4]
+    } 0%, ${theme.colors[theme.primaryColor][6]} 100%)`;
+  }
+  return "none";
+}
+
+function getDescriptionColorVariant(variant: any, theme: any) {
+  if (variant === "gradient") {
+    return theme.black;
+  } if (variant === "white") {
+    return theme.white;
+  }
+  return "";
 }
 
 const useStyles = createStyles((theme, { variant }: ContactIconStyles) => ({
@@ -25,48 +50,29 @@ const useStyles = createStyles((theme, { variant }: ContactIconStyles) => ({
 
   icon: {
     marginRight: theme.spacing.md,
-    backgroundImage:
-      variant === "gradient"
-        ? `linear-gradient(135deg, ${theme.colors[theme.primaryColor][4]} 0%, ${
-          theme.colors[theme.primaryColor][6]
-        } 100%)`
-        : variant === "legal"
-          ? `linear-gradient(135deg, ${theme.colors[theme.primaryColor][4]} 0%, ${
-            theme.colors[theme.primaryColor][6]
-          } 100%)`
-          : "none",
+    backgroundImage: getBgVariant(variant, theme),
     backgroundColor: "transparent",
     color: theme.white,
   },
 
   title: {
-    color:
-      variant === "gradient"
-        ? theme.colors.gray[6]
-        : variant === "white"
-          ? theme.colors[theme.primaryColor][0]
-          : "",
+    color: getColorVariant(variant, theme),
   },
 
   description: {
-    color:
-      variant === "gradient"
-        ? theme.black
-        : variant === "white"
-          ? theme.white
-          : "",
+    color: getDescriptionColorVariant(variant, theme),
   },
 }));
 
 interface ContactIconProps
   extends Omit<React.ComponentPropsWithoutRef<"div">, "title"> {
   icon: React.FC<any>;
-  title: React.ReactNode;
+  title: string;
   type?: "mail" | "phone" | "place";
   description: React.ReactNode;
   variant?: ContactIconVariant;
 }
-// TODO: make hrefs to contact info (mail, phone, place)
+
 function ContactIcon({
   icon: Icon,
   title,
@@ -77,7 +83,6 @@ function ContactIcon({
   ...others
 }: ContactIconProps) {
   const { classes, cx } = useStyles({ variant });
-
   function GradientVariant() {
     return (
       <ThemeIcon size={40} radius="md" className={classes.icon}>
@@ -101,7 +106,6 @@ function ContactIcon({
       </Box>
     );
   }
-
   return (
     <div className={cx(classes.wrapper, className)} {...others}>
       {variant === "gradient" && <GradientVariant />}
@@ -122,25 +126,15 @@ function ContactIcon({
   );
 }
 
-interface ContactIconsListProps {
+export default function ContactIconsList({
+  data = IMPRESSUM_DATA,
+  variant,
+}: {
   data?: ContactIconProps[];
   variant?: ContactIconVariant;
-}
-
-const MOCKDATA = [
-  { title: "Email", description: "kontakt@rekrabice.cz", icon: IconAt },
-  { title: "Telefon", description: "(+420) 777 777 777", icon: IconPhone },
-  { title: "Sídlo", description: "844 Morris Park avenue", icon: IconMapPin },
-  { title: "Pracovní doba", description: "8:00 – 12!00", icon: IconSun },
-  { title: "IČO", description: "87654321", icon: IconSectionSign },
-];
-
-export default function ContactIconsList({
-  data = MOCKDATA,
-  variant,
-}: ContactIconsListProps) {
-  const items = data.map((item, index) => (
-    <ContactIcon key={index} variant={variant} {...item} />
+}) {
+  const items = data.map((item) => (
+    <ContactIcon key={item.title} variant={variant} {...item} />
   ));
   return <Stack>{items}</Stack>;
 }
