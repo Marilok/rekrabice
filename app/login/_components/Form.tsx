f4aa19677cc144861a753e304e37e86bd52d9f3d
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-06-07 20:15:36 +0000

    location.origin emailredirectto

--- app/login/_components/Form.tsx
+++ app/login/_components/Form.tsx
@@ -19,6 +19,18 @@ interface FormValues {
   rememberMe: boolean;
 }
 
+// function getURL() {
+//   let url =
+//     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+//     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+//     "http://localhost:3000/";
+//   // Make sure to include `https://` when not localhost.
+//   url = url.includes("http") ? url : `https://${url}`;
+//   // Make sure to including trailing `/`.
+//   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+//   return url;
+// }
+
 export default function Form() {
   const router = useRouter();
   const supabase = createClientComponentClient();
@@ -26,7 +38,8 @@ export default function Form() {
     await supabase.auth.signInWithOtp({
       email: emailProp,
       options: {
-        emailRedirectTo: "https://rekrabice.cz/auth/callback",
+        emailRedirectTo: `${location.origin}/auth/callback`,
+        // emailRedirectTo: getURL(),
       },
     });
     router.refresh();
