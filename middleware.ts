07164458b7d59405026f1cb4c0f2980e947bc4fb
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-06-07 10:59:10 +0000

    middleware conditional redirects

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,20 @@ import { NextResponse, type NextRequest } from 'next/server';
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  await supabase.auth.getSession()
+  const {data} = await supabase.auth.getSession()
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith('/system') && !data.session) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (pathname === '/system' && data.session) {
+    return NextResponse.redirect(new URL('/system/prijmout', req.url));
+  }
+  
   return res
 }
+
+export const config = {
+  matcher: '/system/:path*',
+};
