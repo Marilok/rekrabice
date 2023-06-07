603b3ecbef4444c4209f9f474ca6378c733748b5
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-06-07 20:14:57 +0000

    lint fix

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,25 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse, type NextRequest } from 'next/server';
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  const {data} = await supabase.auth.getSession()
+  const { data } = await supabase.auth.getSession()
   const { pathname } = req.nextUrl;
 
-  if (pathname.startsWith('/system') && !data.session) {
-    return NextResponse.redirect(new URL('/login', req.url));
+  if (pathname.startsWith("/system") && !data.session) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  if (pathname === '/system' && data.session) {
-    return NextResponse.redirect(new URL('/system/prijmout', req.url));
-  } else if (pathname === '/login' && data.session){
-    return NextResponse.redirect(new URL('/system/prijmout', req.url));
+  if (pathname === "/system" && data.session) {
+    return NextResponse.redirect(new URL("/system/prijmout", req.url));
+  } else if (pathname === "/login" && data.session){
+    return NextResponse.redirect(new URL("/system/prijmout", req.url));
 }
   
   return res
 }
 
 export const config = {
-  matcher: ['/system/:path*', '/login/:path*'],
+  matcher: ["/system/:path*", "/login/:path*"],
 };
