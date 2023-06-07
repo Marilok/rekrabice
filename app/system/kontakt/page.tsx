8d0fc3cb47fc9663dc4137a79b51a35eeb90048b
Marilokms <marilokms@protonmail.com>
2023-06-07 10:58:23 +0000

    double layout remove

--- app/system/kontakt/page.tsx
+++ app/system/kontakt/page.tsx
@@ -1,24 +1,21 @@
 import { Anchor, Space, Text } from "../../mantineClientComponents";
-import StoreLayout from "../layout";
 
 export default async function ContactPage() {
   return (
-    <StoreLayout>
+    <>
       <Text weight="bold">Spěchá to hodně?</Text>
       <Text>
-        Zavolejte prosím Tadeášovi
-        {" "}
+        Zavolejte prosím Tadeášovi{" "}
         <Anchor href="tel:+420606834894">+420 606 834 894</Anchor>
       </Text>
       <Space h="md" />
       <Text weight="bold">Nespěchá to tolik?</Text>
       <Text>
-        Pošlete prosím email Tadeášovi na
-        {" "}
+        Pošlete prosím email Tadeášovi na{" "}
         <Anchor href="mailto:tadeas.bibr@rekrabice.cz">
           tadeas.bibr@rekrabice.cz
         </Anchor>
       </Text>
-    </StoreLayout>
+    </>
   );
 }
