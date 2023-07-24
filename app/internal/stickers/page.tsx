0951e31a2141ba505ff0ef773d2c495d88caad73
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-07-24 20:31:24 +0000

    lint and removal of loading state

--- app/internal/stickers/page.tsx
+++ app/internal/stickers/page.tsx
@@ -3,17 +3,16 @@
 import { Button } from "@mantine/core";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { jsPDF } from "jspdf";
-import { useState } from "react";
 
 export default async function Page() {
-  const [loading, setLoading] = useState(false);
-
   const supabase = createClientComponentClient();
 
   const generatePdf = async (data: any) => {
+    //  TODO: igoners that the constructor doesnt have upperase
+    //  @ts-igonre
     const doc = new jsPDF({
       orientation: "landscape",
-      format: "A9", //52x37 mm
+      format: "A9", // 52x37 mm
     });
 
     data.forEach((item: any, index: any) => {
@@ -23,7 +22,7 @@ export default async function Page() {
 
       doc.text("NEODLEPOVAT", PAGE_WIDTH / 2, SAFE_ZONE * 1.8, {
         align: "center",
-      }); //Beware that it is in landscape mode
+      }); //  Beware that it is in landscape mode
 
       doc.addImage(
         `https://barcodeapi.org/api/128/${item.tracking_id}?text=none`,
@@ -40,33 +39,28 @@ export default async function Page() {
         {
           align: "center",
         },
-      ); //Beware that it is in landscape mode
+      ); // Beware that it is in landscape mode
     });
 
     doc.save("stickers_to_print.pdf");
   };
 
   const handleClick = async () => {
-    setLoading(true);
-    let { data: boxes, error } = await supabase
+    const { data: boxes, error } = await supabase
       .from("boxes")
       .select("tracking_id")
       .eq("batch_id", 1);
 
     console.log(error || "Loaded data succesfully");
     generatePdf(boxes);
-    setLoading(false);
   };
   return (
-    <>
-      <Button
-        onClick={handleClick}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2"
-        loading={loading}
-      >
-        Generovat etikety
-      </Button>
-    </>
+    <Button
+      onClick={handleClick}
+      className="absolute top-1/2 left-1/2 -translate-x-1/2"
+    >
+      Generovat etikety
+    </Button>
   );
 }
 
