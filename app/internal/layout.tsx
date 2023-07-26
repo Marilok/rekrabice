395580f539fd118db2186f40e3806b612f966a98
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-07-26 19:50:25 +0200

    metadata

--- app/internal/layout.tsx
+++ app/internal/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  //   title: "Interní aplikace",
-  //   description: "Interní aplikace pro trasování ReKrabic.",
+  title: "Interní aplikace",
+  description: "Interní aplikace pro výpomoc týmu za ReKrabicemi.",
   robots: "none, noimageindex",
 };
 
