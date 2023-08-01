2c54520665bcd0d20583c4a55ac84a5eaf25ce1c
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-08-02 00:09:11 +0200

    Internal app withou session redirect

--- middleware.ts
+++ middleware.ts
@@ -2,28 +2,45 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
-  const { data } = await supabase.auth.getSession()
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  const { data } = await supabase.auth.getSession();
   const { pathname, searchParams } = req.nextUrl;
+
+  if (pathname.startsWith("/internal") && !data.session) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (pathname === "/internal" && data.session) {
+    return NextResponse.redirect(new URL("/internal/scanner", req.url));
+  }
 
   if (pathname.startsWith("/system") && !data.session) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  if (pathname === "/auth/callback" && searchParams.get("code")) {
-    return NextResponse.redirect(new URL(`/login?code=${searchParams.get("code")}`, req.url));
-  } 
-
   if (pathname === "/system" && data.session) {
     return NextResponse.redirect(new URL("/system/prijmout", req.url));
-  } else if (pathname === "/login" && data.session){
+  }
+
+  if (pathname === "/login" && data.session) {
     return NextResponse.redirect(new URL("/system/prijmout", req.url));
-}
-  
-  return res
+  }
+
+  if (pathname === "/auth/callback" && searchParams.get("code")) {
+    return NextResponse.redirect(
+      new URL(`/login?code=${searchParams.get("code")}`, req.url),
+    );
+  }
+
+  return res;
 }
 
 export const config = {
-  matcher: ["/system/:path*", "/login/:path*", "/auth/:path*"],
+  matcher: [
+    "/system/:path*",
+    "/login/:path*",
+    "/auth/:path*",
+    "/internal/:path*",
+  ],
 };
