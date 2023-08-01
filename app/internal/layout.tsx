c71c450caa1fece1e87beae943d5d7b543c7b21f
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-08-01 21:52:08 +0200

    Internal apps layout

--- app/internal/layout.tsx
+++ app/internal/layout.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from "next";
 import React from "react";
 
+import { AppShell } from "../mantineClientComponents";
+import StyledNavbar from "./_components/Navbar";
+
 export const metadata: Metadata = {
   title: "Interní aplikace",
   description: "Interní aplikace pro výpomoc týmu za ReKrabicemi.",
@@ -12,5 +15,21 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  return <main>{children}</main>;
+  return (
+    <AppShell
+      // @ts-ignore TODO
+      navbar={<StyledNavbar />}
+      styles={{
+        main: {
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        },
+      }}
+      layout="alt"
+    >
+      {children}
+    </AppShell>
+  );
 }
