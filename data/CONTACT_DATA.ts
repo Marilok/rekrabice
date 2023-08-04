8b1e638b43f8aff5c78d5487185dc223bdd341f1
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-08-04 22:13:48 +0200

    update contact data

--- data/CONTACT_DATA.ts
+++ data/CONTACT_DATA.ts
@@ -1,40 +1,36 @@
-import {
-  IconAt,
-  IconMapPin
-} from "@tabler/icons-react";
+import { IconAt, IconMapPin } from "@tabler/icons-react";
 
 export const CONTACT = {
   bankAccountFull: "200200/5300",
   bankAccountNumber: 123456796,
   bankAccountBank: 5500,
-  legalName: "Go Circular s.r.o.",
+  legalName: "Awoxo s.r.o.",
   zip: "60200",
   city: "Brno",
   ico: 3000,
   dic: 3000,
-  address: "Nové sady 303/3",
-
-}
+  address: "Nové sady 988/2",
+  faviconUrl: "https://rekrabice.cz/favicon.svg",
+};
 
 export const contactData = [
-    {
-      title: "Email",
-      description: "marek.svitek@rekrabice.cz",
-      icon: IconAt,
-      // type: "mail",
-    },
-    // {
-    //   title: "Telefon",
-    //   description: "(+420) 777 777 777",
-    //   icon: IconPhone,
-    //   // type: "phone",
-    // },
-    {
-      title: "Sídlo",
-      description: "Zatím hledáme (v Brně)",
-      icon: IconMapPin,
-      // type: "place",
-    },
-    // { title: "Pracovní doba", description: "8:00 - 12:00", icon: IconSun },
-  ];
-  
+  {
+    title: "Email",
+    description: "marek.svitek@rekrabice.cz",
+    icon: IconAt,
+    // type: "mail",
+  },
+  // {
+  //   title: "Telefon",
+  //   description: "(+420) 777 777 777",
+  //   icon: IconPhone,
+  //   // type: "phone",
+  // },
+  {
+    title: "Sídlo",
+    description: "Zatím hledáme (v Brně)",
+    icon: IconMapPin,
+    // type: "place",
+  },
+  // { title: "Pracovní doba", description: "8:00 - 12:00", icon: IconSun },
+];
