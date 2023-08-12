540e0dcbe374ddf6d1807c6d83d33bba672b2213
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-08-12 15:02:35 +0200

    conditional middleware

--- middleware.ts
+++ middleware.ts
@@ -38,11 +38,6 @@ import { NextResponse, type NextRequest } from "next/server";
 //   ],
 // };
 
-// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-// import { NextResponse } from "next/server";
-
-// import type { NextRequest } from "next/server";
-
 // eslint-disable-next-line import/prefer-default-export
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
@@ -59,6 +54,18 @@ export async function middleware(req: NextRequest) {
 
   if (pathname === "/internal" && data.session) {
     return NextResponse.redirect(new URL("/internal/scanner", req.url));
+  }
+
+  if (pathname.startsWith("/system") && !data.session) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (pathname === "/system" && data.session) {
+    return NextResponse.redirect(new URL("/system/prijmout", req.url));
+  }
+
+  if (pathname === "/login" && data.session) {
+    return NextResponse.redirect(new URL("/system/prijmout", req.url));
   }
 
   return res;
