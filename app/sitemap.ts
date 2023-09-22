47ce679a75df84586270d92856db2de07cc2097f
Marilokms <42029758+Marilok@users.noreply.github.com>
2023-09-22 19:19:47 +0200

    update for new site

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,45 @@
+import { DOMAIN } from "data/const";
 import { MetadataRoute } from "next";
-import { DOMAIN } from "../data/const";
+
+// reference: https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap#generate-a-sitemap
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: DOMAIN,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
     },
     {
       url: `${DOMAIN}/o-nas`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
     {
       url: `${DOMAIN}/kontakt`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
     {
       url: `${DOMAIN}/napsali-o-nas`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${DOMAIN}/vratit`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${DOMAIN}/login`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.2,
     },
   ];
 }
