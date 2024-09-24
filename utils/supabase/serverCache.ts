e7f65b2054501e2404918037b365aa1166668bc6
Marilokms <42029758+Marilok@users.noreply.github.com>
2024-09-24 19:02:39 +0200

    server chache

--- utils/supabase/serverCache.ts
+++ utils/supabase/serverCache.ts
@@ -2,13 +2,32 @@ import { Database } from "@/types/supabase";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export default function createClientRouter() {
+// ! This is different fron server client, because it replacec the supabase fetch() function with the nextjs one
+// To be used when needed to store cache
+
+export const createFetch =
+  (options: Pick<RequestInit, "next" | "cache">) =>
+  (url: RequestInfo | URL, init?: RequestInit) =>
+    fetch(url, {
+      ...init,
+      ...options,
+    });
+
+export default function createClientServerCache() {
   const cookieStore = cookies();
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      global: {
+        fetch: createFetch({
+          next: {
+            revalidate: 300, // in seconds
+            tags: ["supabase"],
+          },
+        }),
+      },
       cookies: {
         getAll() {
           return cookieStore.getAll();
